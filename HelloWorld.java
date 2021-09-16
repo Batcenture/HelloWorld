7b75fe39a7afe16040e383aeba0d2c1f0ef42fe7
Batcenture <87466919+Batcenture@users.noreply.github.com>
2021-09-16 09:55:11 +0800

    Update HelloWorld.java

--- HelloWorld.java
+++ HelloWorld.java
@@ -1,5 +1,5 @@
 public class HelloWorld{
 	public static void main(String[] args) {
-		System.out.println("------**** Hellow World uWu ****-----");
+		System.out.println("------**** Hellow World uWu uWu uWu Changes****-----");
 	}
 }
